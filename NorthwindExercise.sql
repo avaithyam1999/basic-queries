a3957b5c2ca9b5cc42189f932b0047e51d006447
Ajith V <132175339+avaithyam1999@users.noreply.github.com>
2025-11-18 13:44:59 -0500

    Add files via upload
    init commit innit?

---
+++ NorthwindExercise.sql
@@ -0,0 +1,4 @@
+-- 1. What is the name of the table that holds the items Northwind sells?
+select productName from Products;
+
+-- 2. Write a query to list the product id, product name, and unit price of every product.
